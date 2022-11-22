e1280027a337ab5713d8260a95a1f7e5aea1d17b
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-22 12:13:52 +0530

    [Siddharth]Add:Binary Search the Word from Word List

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace DataStructureAlgorithm
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinarySearch binary = new BinarySearch();
+            binary.ReadTextFile(@"E:\Bridgelabz\DataStructureAlgorithm\DataStructureAlgorithm\Files\BinarySearch.txt");
+            binary.BinarySearchOperation("Language");
+        }
+    }
+}
